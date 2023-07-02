1f79a7888a4149dcfebb92bd3b0d05646d5fd5d2
Molagare Modiba <molagare@gmail.com>
2023-07-02 15:25:39 +0200

    first commit.completed challenge 6-1

---
+++ challange6-1.js
@@ -0,0 +1,13 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+const isValid = true
+const primaryValid = '0748105141'
+
+if (isValid) {
+    const secondaryValid = '0219131568'
+   console.log('Primary phone is valid numerical string:',primaryValid) 
+   console.log('Secondary phone is valid numerical string:', secondaryValid)
+}
+
